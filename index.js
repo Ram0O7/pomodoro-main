69563482af3263c940108f32755b1588277e5ab5
Ramkrishn <ram706860@gmail.com>
2023-02-24 13:01:10 +0530

    improved ux/ui

--- index.js
+++ index.js
@@ -7,9 +7,69 @@ const timeContainer = document.querySelector('.time-container')
 const timerStatus = document.querySelector('.status')
 const modelOverlay = document.querySelector('.overlays')
 const settingBtn = document.querySelector('.settings')
+const settingControl = document.querySelector('.apply-setting')
+const pomodoroInput = document.getElementById('pomodoro')
+const shortBreakInput = document.getElementById('short-break')
+const longBreakInput = document.getElementById('long-break')
+const fonts = document.querySelectorAll(".font")
+const colors = document.querySelectorAll(".color")
+const timeSlots = document.querySelectorAll(".btn")
 
-const setPomodoro = 5
-const theme = document.documentElement.classList
+//get theme and font from local storage
+const getStorageTheme = () => {
+    let theme = "theme1"
+    if (localStorage.getItem("theme")) {
+        theme = localStorage.getItem("theme")
+        return theme
+    }
+    return theme
+}
+
+const getStorageFont = () => {
+    let font = "Poppins"
+    if (localStorage.getItem("font")) {
+        font = localStorage.getItem("font")
+        return font
+    }
+    return font
+}
+
+//getting initial font and color
+colors.forEach(color => {
+    if (color.id === getStorageTheme()) {
+        color.innerHTML = "&check;"
+    }
+})
+fonts.forEach(font => {
+    if (font.style.fontFamily === getStorageFont()) {
+        font.classList.add("font-active")
+    }
+})
+
+//time variables
+let setPomodoro = 25
+let shortBreak = 5
+let longBreak = 15
+
+settingControl.addEventListener("click", () => {
+    modelOverlay.style.display = "none"
+})
+
+const handleInputChange = (e, type) => {
+    if (type === "pomodoro")
+        setPomodoro = +e.target.value
+    if (type === "short-break")
+        shortBreak = +e.target.value
+    if (type === "long-break")
+        longBreak = +e.target.value
+
+    console.log(setPomodoro, shortBreak, longBreak)
+}
+// changing setting based on the input
+pomodoroInput.addEventListener("change", (e) => handleInputChange(e, "pomodoro"))
+shortBreakInput.addEventListener("change", (e) => handleInputChange(e, "short-break"))
+longBreakInput.addEventListener("change", (e) => handleInputChange(e, "long-break"))
+
 let m = setPomodoro
 const totalSec = setPomodoro * 60
 let s = 0
@@ -24,7 +84,6 @@ settingBtn.addEventListener("click", () => {
 // close model on close button press
 closeModel.addEventListener("click", () => {
     modelOverlay.style.display = "none"
-    console.log("close button clicked")
 })
 
 //countdown logic
@@ -72,5 +131,24 @@ const stopPomodoro = (isOn) => {
 }
 
 timeContainer.addEventListener("click", () => stopPomodoro(isTimerOn))
-// changing theme
-theme.add("theme1")
+// changing font and theme of the page
+fonts.forEach((font, index) => {
+    font.addEventListener("click", () => {
+        document.documentElement.style.setProperty("--font-face", font.style.fontFamily)
+        localStorage.setItem("font", font.style.fontFamily)
+        fonts.forEach(font => font.classList.remove("font-active"))
+        font.classList.add("font-active")
+    })
+})
+
+colors.forEach((color, index) => {
+    color.addEventListener("click", () => {
+        document.documentElement.className = color.id
+        localStorage.setItem("theme", color.id)
+        colors.forEach(color => color.innerHTML = "")
+        color.innerHTML = "&check;"
+    })
+})
+
+document.documentElement.className = getStorageTheme()
+document.documentElement.style.setProperty("--font-face", getStorageFont())
